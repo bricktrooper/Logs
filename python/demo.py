bcbcfbfcd67fe0cb1c97d61b9f87fc0cd3afbda3
bricktrooper <kylepinto@yahoo.ca>
2021-05-03 22:17:27 -0400

    Python: Logs tracing

--- python/demo.py
+++ python/demo.py
@@ -1,5 +1,15 @@
 import log
 
+log.trace_file(False)
+log.trace_line(False)
+log.trace_caller(True)
+log.disable()
+log.enable()
+
+def test():
+	log.error("Asd")
+
+test()
 log.error("ERROR")
 log.warning("WARNING")
 log.success("SUCCESS")
--- python/log.py
+++ python/log.py
@@ -1,6 +1,7 @@
 # ===================== LOGGING ===================== #
 
 import colours
+import inspect
 
 COLOURS = {
 	"error":     colours.RED,
@@ -9,7 +10,8 @@ COLOURS = {
 	"debug":     colours.BLUE,
 	"info":      colours.CYAN,
 	"note":      colours.MAGENTA,
-	"reset":     colours.RESET
+	"reset":     colours.RESET,
+	"trace":     colours.BLACK
 }
 
 PREFIXES = {
@@ -21,23 +23,67 @@ PREFIXES = {
 	"note":      "@"
 }
 
-def format(level, message):
-	return COLOURS[level] + PREFIXES[level] + " " + COLOURS["reset"] + str(message)
+TRACE = {
+	"file":     False,
+	"line":     False,
+	"caller":   False
+}
+
+def trace_file(enabled):
+	TRACE["file"] = enabled
+
+def trace_line(enabled):
+	TRACE["line"] = enabled
+
+def trace_caller(enabled):
+	TRACE["caller"] = enabled
+
+def enable():
+	global DISABLED
+	DISABLED = False
+
+def disable():
+	global DISABLED
+	DISABLED = True
+
+def format(message):
+	if DISABLED:
+		return ""
+
+	stack_trace = inspect.stack()
+	level = stack_trace[1][3]
+
+	if TRACE["file"]:
+		file = str(stack_trace[2][1]) + ":"
+	else:
+		file = ""
+
+	if TRACE["line"]:
+		line = str(stack_trace[2][2]) + ":"
+	else:
+		line = ""
+
+	if TRACE["caller"]:
+		caller = str(stack_trace[2][3]) + ":"
+	else:
+		caller = ""
+
+	return "{}{}{} {}{}{} {}\r\n".format(file, line, caller, COLOURS[level], PREFIXES[level], COLOURS["reset"], str(message))
 
 def error(message):
-	print(format("error", message))
+	print(format(message), end="")
 
 def warning(message):
-	print(format("warning", message))
+	print(format(message), end="")
 
 def success(message):
-	print(format("success", message))
+	print(format(message), end="")
 
 def debug(message):
-	print(format("debug", message))
+	print(format(message), end="")
 
 def info(message):
-	print(format("info", message))
+	print(format(message), end="")
 
 def note(message):
-	print(format("note", message))
+	print(format(message), end="")
