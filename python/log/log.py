97092c1c0f829a5458be8f919bd69705f6bbd9c9
bricktrooper <kylepinto@yahoo.ca>
2022-07-14 14:18:44 -0400

    Python: Package for lite

--- python/log/log.py
+++ python/log/log.py
@@ -1,10 +1,4 @@
-RED     = "\033[0;31m"
-GREEN   = "\033[0;32m"
-YELLOW  = "\033[0;33m"
-BLUE    = "\033[0;34m"
-MAGENTA = "\033[0;35m"
-CYAN    = "\033[0;36m"
-RESET   = "\033[0m"
+from .colours import *
 
 def error(message):
 	print(f"{RED}X{RESET} {message}")
