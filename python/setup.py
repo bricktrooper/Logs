96fd0725ce3859d33998d99b17fd91604b54c95a
bricktrooper <kylepinto1018@gmail.com>
2023-07-26 00:08:21 -0400

    Bump to dev version

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name = "log",
-	version = "0.0.0",
+	version = "0.1.0",
 	description = "A simple logging module with multiple log levels and colours.",
 	author = "Kyle Pinto",
 	author_email = "kylepinto1018@gmail.com",
