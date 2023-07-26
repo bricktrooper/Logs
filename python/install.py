9a2e512bbf68a4e2197f9a34ad4af2c94dcc8703
bricktrooper <kylepinto1018@gmail.com>
2023-07-26 00:12:05 -0400

    temp repo naming

--- python/install.py
+++ python/install.py
@@ -1,9 +1,9 @@
 import os
 import shutil
 
-os.system("git clone https://github.com/bricktrooper/Logs")
-os.system("pip install Logs/python/")
-shutil.rmtree("Logs/", ignore_errors = True)
+os.system("git clone https://github.com/bricktrooper/Logs repo")
+os.system("pip install repo/python/")
+shutil.rmtree("repo/", ignore_errors = True)
 shutil.rmtree("build", ignore_errors = True)
 shutil.rmtree("dist/", ignore_errors = True)
 shutil.rmtree("log.egg-info", ignore_errors = True)
