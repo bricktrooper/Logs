d34ce00a98df06bd3a2978ad7865c663561101f9
bricktrooper <kylepinto1018@gmail.com>
2023-07-26 00:10:09 -0400

    Remove upgrade flag

--- python/install.py
+++ python/install.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 os.system("git clone https://github.com/bricktrooper/Logs")
-os.system("pip install Logs/python/ --upgrade")
+os.system("pip install Logs/python/")
 shutil.rmtree("Logs/", ignore_errors = True)
 shutil.rmtree("build", ignore_errors = True)
 shutil.rmtree("dist/", ignore_errors = True)
